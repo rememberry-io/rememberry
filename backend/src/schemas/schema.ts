8c5b25ef6143fd0c9edbaefc77f5422af70bd60c
Lennart <115736797+lnart@users.noreply.github.com>
2023-09-06 10:21:59 +0200

    FEAT: add relations to tables

--- backend/src/schemas/schema.ts
+++ backend/src/schemas/schema.ts
@@ -6,41 +6,57 @@ CREATE TABLE users(
     username VARCHAR NOT NULL,
     email VARCHAR UNIQUE NOT NULL,
     password VARCHAR NOT NULL
+    PRIMARY KEY(user_id)
 );
-`
+`;
 
 const createStackTableQuery = `
-    CREATE TABLE stacks (
-        stack_id uuid UNIQUE NOT NULL,
-        stack_name VARCHAR DEFAULT='stack',
-        number_of_cards SMALLINT,
-        number_of_learned_cards SMALLINT,
-        number_of_unlearned_cards SMALLINT,
-        topic VARCHAR NOT NULL,
-        total_downloads INT,
-        creator VARCHAR NOT NULL,
-        created_at DATE NOT NULL,
-        last_learned_at DATE NOT NULL,
+CREATE TABLE stacks (
+    stack_id uuid UNIQUE NOT NULL,
+    user_id uuid NOT NULL,
+    stack_name VARCHAR DEFAULT='stack',
+    number_of_cards SMALLINT,
+    number_of_learned_cards SMALLINT,
+    number_of_unlearned_cards SMALLINT,
+    topic VARCHAR NOT NULL,
+    total_downloads INT,
+    creator VARCHAR NOT NULL,
+    created_at DATE NOT NULL,
+    last_learned_at DATE NOT NULL,
+    PRIMARY KEY(stack_id),
+    FOREIGN KEY(user_id) REFERNECES users(user_id)
 
     );
-`
-
-const createPeersTableQuery = `
-        CREATE TABLE peers(
-            peer_id UUID UNIQUE NOT NULL,
-            number_of_members SMALLINT NOT NULL,
-            peer_name VARCHAR,
-
-        );
-`
+`;
 
 const createFlashcardsTableQuery = `
-            CREATE TABLE flashcards(
-                flashcard_id UUID UNIQUE NOT NULL,
-                frontside VARCHAR NOT NULL,
-                backside VARCHAR NOT NULL,
-                learning_status SMALLINT NOT NULL
+CREATE TABLE flashcards(
+    flashcard_id UUID UNIQUE NOT NULL,
+    stack_id UUID NOT NULL,
+    frontside VARCHAR NOT NULL,
+    backside VARCHAR NOT NULL,
+    learning_status SMALLINT NOT NULL,
+    PRIMARY KEY(flashcard_id),
+    FOREIGN KEY(stack_id) REFERENCES stacks(stack_id)
             );
-`
+`;
+
+const createPeersTableQuery = `
+CREATE TABLE peers(
+    peer_id UUID UNIQUE NOT NULL,
+    number_of_members SMALLINT NOT NULL,
+    peer_name VARCHAR,
+        );
+`;
+
+const usersPeersTableQuery = `
+CREATE TABLE users_peers(
+    user_id uuid NOT NULL,
+    peer_id uuid NOT NULL,
+    points SMALLINT,
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+    FOREIGN KEY(peer_id) REFERENCES peers(peer_id)
+            )
+`;
 
 
