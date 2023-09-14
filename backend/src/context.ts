b0bceebc1e238ff7570e5b9a5888bc571c55dc3b
Lennart <115736797+lnart@users.noreply.github.com>
2023-09-14 15:20:39 +0200

    FEAT: add database connection to context

--- backend/src/context.ts
+++ backend/src/context.ts
@@ -1,19 +1,25 @@
-import { CreateHTTPContextOptions } from '@trpc/server/adapters/standalone'
-import { Pool } from 'pg'
+import { CreateHTTPContextOptions } from "@trpc/server/adapters/standalone";
+import { Pool } from "pg";
+
+const dbUser: string = process.env.PG_USERNAME!;
+const dbPassword: string = process.env.PG_PASSWORD!;
+const dbPort: number = +process.env.PG_PORT!;
+const databaseName: string = process.env.PG_DATABASE_NAME!;
+const dbHost: string = process.env.PG_HOST!;
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'cooking',
-    host: 'localhost',
-    database: 'trpcTodoList',
-    port: 5432,
-})
-
+  user: dbUser,
+  password: dbPassword,
+  host: dbHost,
+  database: databaseName,
+  port: dbPort,
+});
 
 export const createContext = (opts: CreateHTTPContextOptions) => {
-    return {
-        db:pool
-    }
-}
+  return {
+    
+    db: pool,
+  };
+};
 
-export type Context = ReturnType<typeof createContext>
+export type Context = ReturnType<typeof createContext>;
