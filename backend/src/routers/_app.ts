7e88dcbd22cc64833063e2cafee440314fc01ea9
Lennart <115736797+lnart@users.noreply.github.com>
2023-09-18 08:12:44 +0200

    FEAT: merge stackrouter in approuter

--- backend/src/routers/_app.ts
+++ backend/src/routers/_app.ts
@@ -1,8 +1,10 @@
+import { stackRouter } from "../stacks/router";
 import { router } from "../trpc";
 import { userRouter } from "../user/router";
 
 export const appRouter = router({
   user: userRouter,
+  stacks: stackRouter,
 });
 
 export type AppRouter = typeof appRouter;
