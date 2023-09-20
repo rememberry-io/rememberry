c9f07ce9326d5d27caa539dfd6b02e776d8142dd
Lennart <115736797+lnart@users.noreply.github.com>
2023-09-20 12:54:23 +0200

    debug

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,17 +1,15 @@
 import { Client } from "pg";
-import { PgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { config } from "dotenv";
 
 config();
 
-// const databaseUrl = process.env.DATABASE_URL || "";
 const pgHost = process.env.PG_HOST || "";
 const pgDatabaseName = process.env.PG_DATABASE_NAME || "";
 const pgUsername = process.env.PG_USERNAME || "";
 const pgPassword = process.env.PG_PASSWORD || "";
 
-const client = new Client({
+export const client = new Client({
   host: pgHost,
   port: 5433,
   user: pgUsername,
@@ -21,4 +19,4 @@ const client = new Client({
 
 await client.connect()
 
-export const db= drizzle(client)
+export const db = drizzle(client)
