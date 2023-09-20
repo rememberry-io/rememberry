c91bcc52aad94c1245bf6dd8c8fcef535c12a6e9
Lennart <115736797+lnart@users.noreply.github.com>
2023-09-20 12:56:21 +0200

    FEAT: import functions to userRouter

--- backend/src/user/router.ts
+++ backend/src/user/router.ts
@@ -1,74 +1,52 @@
 import { z } from "zod";
-import bcrypt from "bcrypt";
 import { router, publicProcedure } from "../trpc";
+import * as userModel from '../user/user.model'
+import * as schema from '../db/schema'
+
+
+const createUserInput = z.object({
+  username: z.string(),
+  email: z.string(),
+  password: z.string()
+})
+const User = z.object({
+  username: z.string(),
+  email: z.string(),
+  password: z.string(),
+  user_id: z.string()
+})
+
 
 export const userRouter = router({
 
-  createUser: publicProcedure
-    .input(
-      z.object({
-        username: z.string(),
-        email: z.string(),
-        password: z.string(),
-      })
-    )
-    .query(async (opts) => {
-      try {
-        const user = opts.input;
-        const salt = await bcrypt.genSalt(10);
-        const hashedPwd = await bcrypt.hash(user.password, salt);
-        const { db } = opts.ctx;
-        const queryValues = [user.username, user.email, hashedPwd];
-        const queryText =
-          "INSERT INTO users(username, email, user_password) VALUES ($1, $2, $3)";
-        const res = await db.query(queryText, queryValues);
-        return res;
-      } catch (error) {
-        console.error(error);
-      }
-    }),
+  createUser: publicProcedure.input(createUserInput).query(async (opts) => {
+      const res = await userModel.createUser(opts.input, opts.ctx.db)
+      return res
+    }
+  ),
 
-  getUserById: publicProcedure.input(z.string()).query(async (opts) => {
-    const { db } = opts.ctx;
-    const userId = opts.input;
-    const queryValues = [userId];
-    const queryText = "SELECT * FROM users WHERE user_id=$1;";
-    const res = await db.query(queryText, queryValues);
-    return res;
+  updateUser: publicProcedure.input(User).query(async(opts)=>{
+    const res = await userModel.updateUserById(opts.input, opts.ctx.db)
+    return res
   }),
 
-  deleteUserById: publicProcedure.input(z.string()).mutation(async (opts) => {
-    const { db } = opts.ctx;
-    const userId: string = opts.input;
-    const values = [userId];
-    const queryText: string = "DELETE FROM users WHERE user_id=$1";
-    const res = await db.query(queryText, values);
-    return res;
+  deleteUserById: publicProcedure.input(User).query(async(opts)=>{
+    const res = await userModel.deleteUserById(opts.input.user_id, opts.ctx.db)
+    return res
   }),
 
-  updateUserById: publicProcedure
-    .input(
-      z.object({
-        username: z.string(),
-        email: z.string(),
-        password: z.string(),
-        userId: z.string()
-      })
-    )
-    .mutation(async (opts) => {
-      const user = opts.input;
-      const { db } = opts.ctx;
-      const queryValues = [
-        user.username,
-        user.email,
-        user.password,
-        user.userId
-      ];
-      const queryText =
-        "UPDATE users SET username=$1, email=$2, password=$3 WHERE user_id=$4";
-      const res = await db.query(queryText, queryValues);
-      return res;
-    }),
-});
+  getAllUsers: publicProcedure.input(z.null()).query(async(opts)=>{
+    const res = await userModel.getAllUsers(opts.ctx.db)
+    return res
+  }),
+
+  getUserById: publicProcedure.input(User).query(async(opts)=>{
+    const res = await userModel.getUserById(opts.input.user_id, opts.ctx.db)
+    return res
+  }),
+
+})
+
+
 
 export type userRouter = typeof userRouter;
