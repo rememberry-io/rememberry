804fb049f083d91ce38e091e3333b086b64312dd
Lennart <115736797+lnart@users.noreply.github.com>
2023-10-02 15:04:56 +0200

    FEAT: fix type bugs

--- backend/src/stacks/router.ts
+++ backend/src/stacks/router.ts
@@ -19,20 +19,6 @@ const userIdInput = z.object({userId:z.string()})
 export const stackRouter = router({
 
     testAuth: privateProcedure.query(async(opts) => { return 'logged in'}),
-
-    getUsersStacksFilteredByTopic: publicProcedure.input(searchByTopicInput).query(async(opts)=>{
-        const res = await stackModels.getUsersStacksFilteredByTopic(opts.input.user_id, opts.input.topic, opts.ctx.db)
-        return res
-    }),
-
-    getUsersStacksFilteredByLearnableCards: publicProcedure.input(userIdInput).query(async(opts) => {
-        const res = await stackModels.getUsersStacksOrderedByLearnableCards(opts.input.userId, opts.ctx.db)
-        return res
-    }),
-
-    createStack: publicProcedure.input(stackInput).query(async(opts) => {
-        const res = await stackModels.createStack(opts.input, opts.input.topic, opts.ctx.db)
-        return res
-    })
+    
 
 })
