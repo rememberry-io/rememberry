3710196d86b75134c14ac0cb4afdc7173a8f04dd
Lennart <115736797+lnart@users.noreply.github.com>
2023-10-03 10:16:05 +0200

    FEAT: write procedures for mapRouter

---
+++ backend/src/endPoints/maps/router.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { router, publicProcedure } from "../../trpc";
+import * as mapModels from './mapModels'
+import * as schema from "../../db/schema";
+
+
+const createMapInput = z.object({
+    user_id: z.string(),
+    map_name: z.string(),
+    map_description: z.string()
+})
+
+const updateMapInput = z.object({
+    map_name: z.string(),
+    map_description: z.string()
+})
+
+
+
+const mapRouter = router({
+
+    createMap: publicProcedure.input(createMapInput).query(async(opts) => {
+        const res = mapModels.createMap(opts.input, opts.ctx.db)
+        return res
+    }),
+
+    getMapsById: publicProcedure.input(z.string()).query(async(opts) => {
+        const res = await mapModels.getMapsByUserId(opts.input, opts.ctx.db)
+        return res 
+    }),
+
+    updateMap: publicProcedure.input(updateMapInput).query(async(opts) => {
+        const res = await mapModels.updateMap(opts.input, opts.ctx.db)
+        return res
+    }), 
+
+    deleteMap: publicProcedure.input(z.string()).query(async (opts) => {
+        const res = await mapModels.deleteMapWithAllStacks(opts.input, opts.ctx.db)
+        return res 
+    })
+
+})
+
+export type MapRouter = typeof mapRouter
