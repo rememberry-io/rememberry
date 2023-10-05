6c815806d1deb3fac9bdd41ef457fe0803d0336f
Lennart <115736797+lnart@users.noreply.github.com>
2023-10-05 21:46:14 +0200

    FEAT: write flashcard types and functions

--- backend/src/endPoints/flashcards/flashcardModels.ts
+++ backend/src/endPoints/flashcards/flashcardModels.ts
@@ -1,10 +1,26 @@
 import { client } from "../../db/db";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from '../../db/schema'
-import { eq, and, desc, sql, } from "drizzle-orm";
+import { eq, and, desc, sql} from "drizzle-orm";
 
 const database = drizzle(client, { schema })
 type dbConnection = typeof database
+
+
+type Flashcards = {
+    frontside: string | null,
+    frontside_media: string | null,
+    frontside_media_positioning: string | null,
+    backside: string | null,
+    backside_media: string | null,
+    backside_media_positioning: string | null
+}
+
+enum LearningStatus {
+    learnable = 1,
+    notLearnable = 0
+}
+
 
 export async function createFlashcard(flashcard: schema.NewFlashcard, db:dbConnection): Promise<schema.Flashcard[]>{
     const prep = db.insert(schema.flashcards).values({
@@ -38,18 +54,77 @@ export async function deleteFlashcard(flashcardId:string, db:dbConnection): Prom
 export async function getAllFlashcardsFromStack(stackId:string, db:dbConnection){
     const prep = db
     .select({
+        flashcard_id: schema.flashcards.flashcard_id,
         frontside: schema.flashcards.frontside_text,
         frontside_media: schema.frontside_media.media_link,
         frontside_media_positioning: schema.frontside_media.positioning,
         backside: schema.flashcards.backside_text,
         backside_media: schema.backside_media.media_link,
-        backside_media_positioning: schema.backside_media.positioning
+        backside_media_positioning: schema.backside_media.positioning,
+        learning_status: schema.session_data.learning_status
         })
     .from(schema.flashcards)
     .leftJoin(schema.frontside_media, eq(schema.flashcards.flashcard_id, schema.frontside_media.flashcard_id))
     .leftJoin(schema.backside_media, eq(schema.flashcards.flashcard_id, schema.backside_media.flashcard_id))
     .where(eq(schema.flashcards.stack_id, sql.placeholder("stackId")))
-    
+    .prepare("getAllFlashcardsFromStack")
+
     const res = await prep.execute({stackId: stackId})
     return res
+}
+
+export async function getLearnableFlashcardsFromStack(stackId:string, db:dbConnection): Promise<Flashcards[]>{
+    const prep = db
+    .select({
+        flashcard_id: schema.flashcards.flashcard_id,
+        frontside: schema.flashcards.frontside_text,
+        frontside_media: schema.frontside_media.media_link,
+        frontside_media_positioning: schema.frontside_media.positioning,
+        backside: schema.flashcards.backside_text,
+        backside_media: schema.backside_media.media_link,
+        backside_media_positioning: schema.backside_media.positioning,
+        learning_status: schema.session_data.learning_status
+    })
+    .from(schema.flashcards)
+    .leftJoin(schema.frontside_media, eq(schema.flashcards.flashcard_id, schema.frontside_media.flashcard_id))
+    .leftJoin(schema.backside_media, eq(schema.flashcards.flashcard_id, schema.backside_media.flashcard_id))
+    .innerJoin(schema.session_data, eq(schema.flashcards.flashcard_id, schema.session_data.flashcard_id))
+    .where(and(eq(schema.flashcards.stack_id, sql.placeholder("stack_id")), eq(schema.session_data.learning_status, LearningStatus.learnable)))
+    .prepare("getLearnableFlashcardsFromStack")
+    
+    const res = await prep.execute({stack_id: stackId})
+    return res
+}
+
+
+
+export async function getAllFlashcardsFromStackAndChildStacks(stackId:string, db:dbConnection):Promise<any>{
+    
+    const res = await db.execute(sql`
+    WITH RECURSIVE cte_stacks AS (
+        SELECT stack_iD
+        FROM stacks
+        WHERE stack_id=${stackId}
+
+        UNION ALL
+
+        SELECT stacks.stack_id
+        FROM stacks
+        JOIN cte_stacks ON stacks.parent_stack_id = cte_stacks.stack_id
+    )
+
+    SELECT
+        flashcards.flashcard_id,
+        flashcards.frontside_text,
+        flashcards.backside_text,
+        frontside_media.media_link,
+        frontside_media.positioning,
+        backside_media.media_link,
+        backside_media.positioning
+    FROM cte_stacks
+    JOIN flashcards ON cte_stacks.stack_id = flashcards.stack_id
+    LEFT JOIN frontside_media ON flashcards.flashcard_id = frontside_media.flashcard_id
+    LEFT JOIN backside_media ON flashcards.flashcard_id = backside_media.flashcard_id
+    `)
+    return res.rows
 }
