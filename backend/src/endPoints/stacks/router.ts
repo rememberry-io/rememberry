bbe8cde54b8693a99594ea7f7bc01e9353967544
Lennart <115736797+lnart@users.noreply.github.com>
2023-10-06 16:31:36 +0200

    CHORE: add stackController for separation of concerns

--- backend/src/endPoints/stacks/router.ts
+++ backend/src/endPoints/stacks/router.ts
@@ -1,12 +1,10 @@
 import { router, publicProcedure } from "../../trpc";
-import * as stackModels from "./stackModels";
 import z from "zod";
 import { config } from "dotenv";
-import * as schema from '../../db/schema'
+import * as schema from "../../db/schema";
+import * as stackController from './stackController'
 
 config();
-
-
 
 const stackInput = z.object({
   stack_id: z.string().optional(),
@@ -17,59 +15,74 @@ const stackInput = z.object({
   map_id: z.string(),
   stack_description: z.string(),
   positioning: z.string().optional(),
-  parent_stack_id: z.string().optional()
-})
+  parent_stack_id: z.string().optional(),
+});
 
 const changeParentStackInput = z.object({
-  child_id : z.string(),
-  new_parent_id: z.string()
-})
-
-
+  child_id: z.string(),
+  new_parent_id: z.string(),
+});
 
 export const stackRouter = router({
-
-  createStack : publicProcedure.input(stackInput).mutation(async(opts) => {
-    const res = await stackModels.createStack(opts.input, opts.ctx.db)
-    return res
+  createStack: publicProcedure.input(stackInput).mutation(async (opts) => {
+    const res = await stackController.controlCreateStack(opts.input, opts.ctx.db);
+    return res;
   }),
 
-  getMapsHighestOrderParents: publicProcedure.input(z.string()).query(async(opts) => {
-    const res = await stackModels.getHighestOrderParentStacks(opts.input, opts.ctx.db)
-    return res 
+  getMapsHighestOrderStacks: publicProcedure
+    .input(z.string())
+    .query(async (opts) => {
+      const res = await stackController.controlGetHighestOrderStacks(
+        opts.input,
+        opts.ctx.db
+      );
+      return res;
+    }),
+
+  getAllStacksFromMap: publicProcedure.input(z.string()).query(async (opts) => {
+    const res = await stackController.controlGetAllStacksFromMap(opts.input, opts.ctx.db);
+    return res;
   }),
 
-  getAllStacksFromMap : publicProcedure.input(z.string()).query(async(opts) => {
-    const res = await stackModels.getStacksFromMap(opts.input, opts.ctx.db)
-    return res
+  getDirectChildsFromParent: publicProcedure.input(z.string()).query(async (opts) => {
+    const res = await stackController.controlGetDirectChildsFromParent(
+      opts.input,
+      opts.ctx.db
+    );
+    return res;
   }),
 
-  getStacksFromParent : publicProcedure.input(z.string()).query(async(opts) => {
-    const res = await stackModels.getStacksFromParent(opts.input, opts.ctx.db)
-    return res
+  getAllChildsFromParent: publicProcedure
+    .input(z.string())
+    .query(async (opts) => {
+      const res = await stackController.controlGetAllChildsFromParent(
+        opts.input,
+        opts.ctx.db
+      );
+      return res;
+    }),
+
+  getParentFromStack: publicProcedure.input(z.string()).query(async (opts) => {
+    const res = await stackController.controlGetParentFromStack(opts.input, opts.ctx.db);
+    return res;
   }),
 
-  getAllChildsFromParent: publicProcedure.input(z.string()).query(async(opts) => {
-    const res = await stackModels.getAllChildsFromParent(opts.input, opts.ctx.db)
-    return res
-  }),
+  changeParentStack: publicProcedure
+    .input(changeParentStackInput)
+    .mutation(async (opts) => {
+      const res = await stackController.controlChangeParentStack(opts.input, opts.ctx.db);
+      return res;
+    }),
 
-  getStackById: publicProcedure.input(z.string()).query(async(opts) => {
-    const res = await stackModels.getParentFromStack(opts.input, opts.ctx.db)
-    return res 
-  }),
+  deleteParentStackRelation: publicProcedure
+    .input(z.string())
+    .mutation(async (opts) => {
+      const res = await stackController.controlDeleteParentStackRelation(
+        opts.input,
+        opts.ctx.db
+      );
+      return res;
+    }),
+});
 
-  changeParentStack: publicProcedure.input(changeParentStackInput).mutation(async(opts) => {
-    const res = await stackModels.changeParentStack(opts.input, opts.ctx.db)
-    return res 
-  }),
-
-  deleteParentStackRelation: publicProcedure.input(z.string()).mutation(async(opts) => {
-    const res = await stackModels.deleteParentStackRelation(opts.input, opts.ctx.db)
-    return res 
-  }),
-  
-})
-
-
-export type stackRouter = typeof stackRouter
+export type stackRouter = typeof stackRouter;
--- backend/src/endPoints/stacks/stackModels.ts
+++ backend/src/endPoints/stacks/stackModels.ts
@@ -1,55 +1,79 @@
 import { client } from "../../db/db";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "../../db/schema";
-import { eq, and, desc, sql, isNull} from "drizzle-orm";
-
+import { eq, and, desc, sql, isNull } from "drizzle-orm";
+import * as types from "./types";
 
 const database = drizzle(client, { schema });
 type dbConnection = typeof database;
 
-
-
-export async function createStack(stack:schema.NewStack, db:dbConnection): Promise<schema.NewStack[]>{
-  const date = new Date()
-  const res = await db.insert(schema.stacks).values({
-    stack_id: stack.stack_id,
-    stack_name: stack.stack_name,
-    number_of_learned_cards: stack.number_of_learned_cards,
-    number_of_unlearned_cards: stack.number_of_unlearned_cards,
-    created_at: date,
-    map_id: stack.map_id,
-    stack_description: stack.stack_description,
-    positioning: stack.positioning,
-    parent_stack_id: stack.parent_stack_id
-  }).returning()
-  return res 
+export async function createStack(
+  stack: schema.NewStack,
+  date: Date,
+  db: dbConnection
+): Promise<schema.NewStack[]> {
+  const res = await db
+    .insert(schema.stacks)
+    .values({
+      stack_id: stack.stack_id,
+      stack_name: stack.stack_name,
+      number_of_learned_cards: stack.number_of_learned_cards,
+      number_of_unlearned_cards: stack.number_of_unlearned_cards,
+      created_at: date,
+      map_id: stack.map_id,
+      stack_description: stack.stack_description,
+      positioning: stack.positioning,
+      parent_stack_id: stack.parent_stack_id,
+    })
+    .returning();
+  return res;
 }
 
-type ParentAndChildId = {
-  child_id : string,
-  new_parent_id : string
+export async function getStacksFromMap(mapId: string, db: dbConnection) {
+  const prep = db
+    .select()
+    .from(schema.stacks)
+    .where(eq(schema.stacks.map_id, sql.placeholder("id")))
+    .prepare("stacks");
+  const res = await prep.execute({ id: mapId });
+  return res;
 }
 
-export async function getStacksFromMap(mapId:string, db:dbConnection){
-  const prep = db.select().from(schema.stacks).where(eq(schema.stacks.map_id, sql.placeholder("id"))).prepare("stacks")
-  const res = await prep.execute({id: mapId})
-  return res 
+export async function getHighestOrderParentStacks(
+  mapId: string,
+  db: dbConnection
+): Promise<schema.Stack[]> {
+  const prep = db
+    .select()
+    .from(schema.stacks)
+    .where(
+      and(
+        eq(schema.stacks.map_id, sql.placeholder("id")),
+        isNull(schema.stacks.parent_stack_id)
+      )
+    )
+    .prepare("highestOrderStacks");
+  const res = await prep.execute({ id: mapId });
+  return res;
 }
 
-export async function getHighestOrderParentStacks(mapId:string, db:dbConnection): Promise<schema.Stack[]>{
-  const prep = db.select().from(schema.stacks).where(and(eq(schema.stacks.map_id, sql.placeholder("id"),), isNull(schema.stacks.parent_stack_id))).prepare("highestOrderStacks")
-  const res = await prep.execute({id: mapId})
-  return res
+export async function getDirectChildsFromParent(
+  parentStackId: string,
+  db: dbConnection
+) {
+  const prep = db
+    .select()
+    .from(schema.stacks)
+    .where(eq(schema.stacks.parent_stack_id, sql.placeholder("id")))
+    .prepare("childStacks");
+  const res = await prep.execute({ id: parentStackId });
+  return res;
 }
 
-export async function getStacksFromParent(parentStackId:string, db:dbConnection){
-  const prep = db.select().from(schema.stacks).where(eq(schema.stacks.parent_stack_id, sql.placeholder("id"))).prepare("childStacks")
-  const res = await prep.execute({id:parentStackId})
-  return res 
-}
-
-
-export async function getAllChildsFromParent(stackId:string, db:dbConnection){
+export async function getAllChildsFromParent(
+  stackId: string,
+  db: dbConnection
+) {
   const res = await db.execute(sql`
   WITH RECURSIVE cte_substacks AS (
       
@@ -63,22 +87,43 @@ export async function getAllChildsFromParent(stackId:string, db:dbConnection){
       )
       SELECT * 
       FROM cte_substacks
-  `)
-  return res.rows
+  `);
+  return res.rows;
 }
 
-export async function getParentFromStack(parentStackId:string, db:dbConnection){
-  const prep = db.select().from(schema.stacks).where(eq(schema.stacks.stack_id, sql.placeholder("id"))).prepare("parentStack")
-  const res = await prep.execute({id: parentStackId})
-  return res 
+export async function getStackById(
+  stackId: string,
+  db: dbConnection
+) {
+  const prep = db
+    .select()
+    .from(schema.stacks)
+    .where(eq(schema.stacks.stack_id, sql.placeholder("id")))
+    .prepare("stack");
+  const res = await prep.execute({ id: stackId });
+  return res;
 }
 
-export async function changeParentStack(userInput:ParentAndChildId, db:dbConnection): Promise<schema.Stack[]>{
-  const res = await db.update(schema.stacks).set({parent_stack_id:userInput.new_parent_id}).where(eq(schema.stacks.stack_id, userInput.child_id)).returning()
-  return res
+export async function changeParentStack(
+  parentAndChild: types.ParentAndChildId,
+  db: dbConnection
+): Promise<schema.Stack[]> {
+  const res = await db
+    .update(schema.stacks)
+    .set({ parent_stack_id: parentAndChild.new_parent_id })
+    .where(eq(schema.stacks.stack_id, parentAndChild.child_id))
+    .returning();
+  return res;
 }
 
-export async function deleteParentStackRelation(childStackId:string, db:dbConnection): Promise<schema.Stack[]>{
-  const res = await db.update(schema.stacks).set({parent_stack_id: null}).where(eq(schema.stacks.stack_id, childStackId)).returning()
-  return res 
+export async function deleteParentStackRelation(
+  childStackId: string,
+  db: dbConnection
+): Promise<schema.Stack[]> {
+  const res = await db
+    .update(schema.stacks)
+    .set({ parent_stack_id: null })
+    .where(eq(schema.stacks.stack_id, childStackId))
+    .returning();
+  return res;
 }
