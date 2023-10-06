0b3d8274a570255052b92179fb9e4b2c5bedade3
Lennart <115736797+lnart@users.noreply.github.com>
2023-10-06 09:43:00 +0200

    CHORE: fix bug

--- backend/src/context.ts
+++ backend/src/context.ts
@@ -3,7 +3,6 @@ import { Client } from "pg";
 import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
 import {client} from './db/db'
 
-await client.connect()
 
 export const db = drizzle(client) as NodePgDatabase<typeof import("/Users/lennartp./Desktop/Projekte/rememberry/backend/src/db/schema.ts")>
 
