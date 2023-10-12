9a23b05284c057ef9ddb330260b35b97709d5aac
Lennart <115736797+lnart@users.noreply.github.com>
2023-10-12 08:35:15 +0200

    FEAT: write types & schemas for login controller, model & router

---
+++ backend/src/endPoints/login/types.ts
@@ -0,0 +1,28 @@
+import z from "zod"
+export type LoginCredentials = {
+    email: string,
+    password: string
+}
+
+export const LoginCredentialsSchema = z.object({
+    email: z.string(),
+    password: z.string()
+})
+
+export type LoginUser = {
+    user_id: string, 
+    password: string
+}
+
+export const refreshTokenInputSchema = z.object({
+    accessToken: z.string(),
+    refreshToken: z.string()
+})
+
+export type refreshTokenInputType = {
+    refreshToken: string
+}
+
+export type JWTPayload = {
+    userId: string
+}
