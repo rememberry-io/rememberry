ec3c8a430811e46c9257c4a3bdfbd17126f5dc79
Lennart <115736797+lnart@users.noreply.github.com>
2023-10-12 08:35:54 +0200

    feat: merge login router

--- backend/src/routers/_app.ts
+++ backend/src/routers/_app.ts
@@ -3,12 +3,14 @@ import { router } from "../trpc";
 import { userRouter } from "../endPoints/user/router";
 import { mapRouter } from "../endPoints/maps/router";
 import { flashcardRouter } from "../endPoints/flashcards/router";
+import { loginRouter } from "../endPoints/login/router";
 
 export const appRouter = router({
   user: userRouter,
   stacks: stackRouter,
   maps: mapRouter,
   flashcards: flashcardRouter,
+  login: loginRouter
 });
 
 export type AppRouter = typeof appRouter;
