ab449a45ed65d4ff252689e42ff348aa37d025d7
Lennart <115736797+lnart@users.noreply.github.com>
2023-10-12 08:37:29 +0200

    FEAT: write middleware for jwt validation

--- backend/src/middleware/jwt.ts
+++ backend/src/middleware/jwt.ts
@@ -2,38 +2,22 @@ import { router, middleware, publicProcedure } from "../trpc";
 import jwt from "jsonwebtoken";
 import { config } from "dotenv";
 import { TRPCError } from "@trpc/server";
+import { signAccessToken } from "../endPoints/login/loginController";
 config();
 
 const isLoggedIn = middleware(async ({ next, ctx }) => {
   const { req } = ctx;
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token)
-    throw new TRPCError({ code: "UNAUTHORIZED", message: "Token is required" });
-
-  const key = process.env.ACCESS_TOKEN_SECRET;
-  if (!key)
-    throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message: "Server configuration error",
-    });
-
-  try {
-    const payload = jwt.verify(token, key);
-    console.log(payload);
-
-    return next({
-      ctx: {
-        payload,
-        test: "hs",
-      },
-    });
-  } catch (err) {
-    console.error(err); // Log the error to console or a logging service
-    throw new TRPCError({
-      code: "UNAUTHORIZED",
-      message: "Invalid or expired token",
-    });
+  const token = req.headers.authorization?.split(' ')[1]
+  if(!token){
+    throw new TRPCError({code: "UNAUTHORIZED"})
   }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!)
+  return next()
 });
+
+const refreshAccessToken = middleware(async ({next, ctx}) => {
+
+  return next()
+})
 
 export const privateProcedure = publicProcedure.use(isLoggedIn);
