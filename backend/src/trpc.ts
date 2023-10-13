11c7c8934d2b9e6c2d3382ce694fe0705f771921
Laurin-Notemann <laurin.notemann@gmail.com>
2023-10-13 13:48:49 +0200

    test

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -3,6 +3,8 @@ import { Context } from "./context";
 
 const t = initTRPC.context<Context>().create();
 
+const rteest = 23;
+
 export const router = t.router;
 export const middleware = t.middleware;
 export const publicProcedure = t.procedure;
