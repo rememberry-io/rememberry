2b6179999e663d28e78872cd285fb198e96a34ec
Laurin-Notemann <laurin.notemann@gmail.com>
2023-10-15 23:20:49 +0200

    fix

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -3,6 +3,7 @@ import { Context } from "./context";
 
 const t = initTRPC.context<Context>().create();
 
+
 export const router = t.router;
 export const middleware = t.middleware;
 export const publicProcedure = t.procedure;
