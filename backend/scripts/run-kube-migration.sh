8a03e61473ac5dba5077c76d2c980afb82db02d6
Laurin-Notemann <laurin.notemann@gmail.com>
2023-10-19 16:01:41 +0200

    fix

--- backend/scripts/run-kube-migration.sh
+++ backend/scripts/run-kube-migration.sh
@@ -1,2 +1,2 @@
-kubectl cp ./drizzle rememberry/rem-db-stateset-0:/tmp/drizzle
-cat kube-execute-sql.sh | kubectl exec -i -n rememberry rem-db-stateset-0 -- sh
+kubectl cp ~/work/rememberry/rememberry/backend/drizzle rememberry/rem-db-stateset-0:/tmp/drizzle
+cat ~/work/rememberry/rememberry/backend/scripts/kube-execute-sql.sh | kubectl exec -i -n rememberry rem-db-stateset-0 -- sh
