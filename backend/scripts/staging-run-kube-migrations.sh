91a8383cf0aa7381f20832b3fdd6a38992c747f3
Laurin-Notemann <laurin.notemann@gmail.com>
2023-10-20 16:44:39 +0200

    cd: fix db staging migration
    fix: change github acitons name

--- backend/scripts/staging-run-kube-migrations.sh
+++ backend/scripts/staging-run-kube-migrations.sh
@@ -1,2 +1,2 @@
-kubectl cp ~/work/rememberry/rememberry/backend/drizzle rememberry/staging-db-set-0 :/tmp/drizzle
+kubectl cp ~/work/rememberry/rememberry/backend/drizzle rememberry/staging-db-set-0:/tmp/drizzle
 cat ~/work/rememberry/rememberry/backend/scripts/kube-execute-sql.sh | kubectl exec -i -n rememberry staging-db-set-0 -- sh
