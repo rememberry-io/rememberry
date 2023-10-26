04334355a8959a5cbcfab439d965c10790f3bc12
Laurin-Notemann <laurin.notemann@gmail.com>
2023-10-26 19:37:13 +0200

    fix: version issues and remove unnecessary files

--- frontend/src/app/testcomponents/page.tsx
+++ frontend/src/app/testcomponents/page.tsx
@@ -1,11 +1,8 @@
-import { BearCounter, Controls } from "./StoreTest";
 import TestButton from "./TestButton";
 
 export default function TestComponent() {
   return (
     <div>
-      <BearCounter />
-      <Controls />
       <TestButton />
     </div>
   );
