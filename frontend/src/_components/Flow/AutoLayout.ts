7c579527df58702d04b4f9411573c15d7b154741
paulocerez <paulo.ramirez@web.de>
2023-10-27 16:46:45 +0200

    feat: Begin implementation of Auto Layout

---
+++ frontend/src/_components/Flow/AutoLayout.ts
@@ -0,0 +1,62 @@
+// import initialEdges from "@/_components/Flow/FlowElements/edges";
+// import initialNodes from "@/_components/Flow/FlowElements/nodes";
+// import Dagre from "@dagrejs/dagre";
+// import { useCallback, useEffect } from "react";
+// import {
+//   Edge,
+//   Node,
+//   useEdgesState,
+//   useNodesState,
+//   useReactFlow,
+// } from "reactflow";
+
+// const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+
+// const getLayoutedElements = (
+//   nodes: Node[],
+//   edges: Edge[],
+//   options: { direction: "TB" | "LR" },
+// ) => {
+//   g.setGraph({ rankdir: options.direction });
+
+//   edges.forEach((edge) => g.setEdge(edge.source, edge.target));
+//   nodes.forEach((node) => g.setNode(node.id, node as any));
+
+//   Dagre.layout(g);
+
+//   return {
+//     nodes: nodes.map((node) => {
+//       const { x, y } = g.node(node.id);
+
+//       return { ...node, position: { x, y } };
+//     }),
+//     edges,
+//   };
+// };
+
+// const LayoutFlow = () => {
+//   const { fitView } = useReactFlow();
+//   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+//   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+//   const onLayout = useCallback(
+//     (direction: "TB" | "LR") => {
+//       const layouted = getLayoutedElements(nodes, edges, { direction: "LR" });
+
+//       setNodes([...layouted.nodes]);
+//       setEdges([...layouted.edges]);
+//     },
+//     [nodes, edges, setEdges, setNodes],
+//   );
+
+//   // Use useEffect to trigger fitView
+//   useEffect(() => {
+//     window.requestAnimationFrame(() => {
+//       fitView();
+//     });
+//   }, [fitView]);
+
+//   // Rest of your component code...
+// };
+
+// export default LayoutFlow;
