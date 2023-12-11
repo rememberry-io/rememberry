3ba4ab827ad5dabac1e709afa62cc4c63f928545
Laurin-Notemann <laurin.notemann@gmail.com>
2023-12-11 17:51:28 +0100

    test formatter

--- backend/src/context.ts
+++ backend/src/context.ts
@@ -2,8 +2,8 @@ import { inferAsyncReturnType } from "@trpc/server";
 import { CreateHTTPContextOptions } from "@trpc/server/adapters/standalone";
 
 export const createContext = (opts: CreateHTTPContextOptions) => {
-  const { req, res } =   opts;
-  
+  const { req, res } = opts;
+
   return {
     req,
     res,
