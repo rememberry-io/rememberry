57358b20cdb2ee9be970069fa1e5169812f9dcc1
Laurin-Notemann <laurin.notemann@gmail.com>
2023-12-22 19:28:21 +0100

    refactor: implement user controller and routes based on lucia auth

---
+++ backend/src/endPoints/user/types.ts
@@ -0,0 +1,34 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { User } from "lucia";
+import { z } from "zod";
+
+export const UpdateUsernameAndEmailRouteInput = z.object({
+  userId: z.string(),
+  email: z.string(),
+  username: z.string(),
+});
+
+export const UpdatePasswordRouteInput = z.object({
+  email: z.string(),
+  newPassword: z.string(),
+});
+
+export type GetUserBySessionInput = {
+  req: IncomingMessage;
+  res: ServerResponse<IncomingMessage>;
+};
+
+export type UpdateUsernameAndEmailInput = {
+  userId: string;
+  username: string;
+  email: string;
+};
+
+export type UpdatePasswordInput = {
+  email: string;
+  newPassword: string;
+};
+
+export type UserControllerOutput = {
+  user: User;
+};
