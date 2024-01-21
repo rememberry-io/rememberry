eadcc8956dbfb7b01d6ee9c0969559fc5a54b94c
Joelheile <joel.heil-escobar@outlook.de>
2024-01-21 17:35:47 +0200

    feat: changed flashcard from button to input

--- frontend/src/components/Flow/FlashcardComponents/Flashcard.tsx
+++ frontend/src/components/Flow/FlashcardComponents/Flashcard.tsx
@@ -34,6 +34,7 @@ export const Flashcard: React.FC<FlashcardProps> = ({ data, id }) => {
 
   const toggleCard = () => {
     setIsFront(!isFront);
+    console.log("changed")
   };
 
   const handleColorChange = (color: ColorType) => {
