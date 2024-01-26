ee554d268962591ccf3b590f245f2671ec9453c4
Laurin-Notemann <laurin.notemann@gmail.com>
2024-01-26 18:13:05 +0100

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,8 @@ const server = createHTTPServer({
     origin: [
       "http://localhost:3000",
       "http://127.0.0.1:3000",
-      "rememberry.app",
-      "web.stage.rememberry.app",
+      "https://rememberry.app",
+      "https://web.stage.rememberry.app",
     ],
     credentials: true,
   }),
