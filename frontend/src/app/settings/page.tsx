cef3904b5836a7f50495db8a6348bbbbafc8f752
Joelheile <joel.heil-escobar@outlook.de>
2024-02-14 17:13:02 +0100

    feat: created new profile settings page

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,3 +1,34 @@
-export default function Settings() {
-  return <div>Settings</div>;
+//profile page
+
+// root page
+"use client";
+import FlowBackground from "@/components/Flow/Background/flowBackground";
+import FlowFooter from "@/components/Flow/CustomComponents/flowFooter";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+import "reactflow/dist/style.css";
+
+function Settings() {
+  return (
+    <div className="relative">
+      <FlowBackground />
+      <div className="z-10 absolute top-0 left-0 w-full h-screen">
+        <FlowFooter>
+          <>
+            <Link href={"/map"}>
+              <Button className="flex items-center ">
+                <span>Go to Map</span>
+                <ArrowRight className="ml-2" />
+              </Button>
+            </Link>
+            {/* <Button onClick={openAddDialog}>Add Map</Button> */}
+          </>
+        </FlowFooter>
+      </div>
+    </div>
+  );
 }
+
+export default Settings;
