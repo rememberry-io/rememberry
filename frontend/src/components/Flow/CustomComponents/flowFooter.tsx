c0722019f703e2c3547810f674d456e7835239de
Joelheile <joel.heil-escobar@outlook.de>
2024-02-23 15:15:19 +0100

    fix: gave footer z index so it is above nodes etc

--- frontend/src/components/Flow/CustomComponents/flowFooter.tsx
+++ frontend/src/components/Flow/CustomComponents/flowFooter.tsx
@@ -3,8 +3,10 @@ import "reactflow/dist/style.css";
 
 const FlowFooter: React.FC<{ children: JSX.Element }> = ({ children }) => {
   return (
-    <div className="fixed inset-x-0 bottom-4 px-4 sm:px-6 lg:px-8">
-      <div className="flex justify-center space-x-4">{children}</div>
+    <div className="fixed z-20 inset-x-0 bottom-4 px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-center space-x-4">
+        {children}
+      </div>
     </div>
   );
 };
