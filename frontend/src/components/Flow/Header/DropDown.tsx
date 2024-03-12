b72493375fd1326e1862b9e2da331e6915168e89
Joelheile <joel.heil-escobar@outlook.de>
2024-03-12 15:26:18 +0100

    feat: dropdown visuals

--- frontend/src/components/Flow/Header/DropDown.tsx
+++ frontend/src/components/Flow/Header/DropDown.tsx
@@ -7,23 +7,39 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { create, useStore } from "zustand";
+import useDropDownFocusStore from "../stores/headerDropdownStore";
+
 
 export function DropDown() {
+  const title = useDropDownFocusStore((state) => state.dropdownName)
+  const updateTitle = useDropDownFocusStore((state) => state.updateDropDown)
+
   return (
     <Select>
       <SelectTrigger className="[w-auto]  dark:bg-dark-800  hover:dark:bg-dark-600 hover:bg-gray-50">
-        <SelectValue placeholder="Difficulty" />
+        <SelectValue placeholder={title} />
       </SelectTrigger>
       <SelectContent className="flex flex-col  dark:bg-dark-800  ">
         <SelectGroup className="">
           <SelectLabel>How do you want to see the difficulty?</SelectLabel>
-          <SelectItem className="" value="apple">
+          <SelectItem
+            onClick={() => updateTitle("Colored Borders")}
+            className=""
+            value="borders"
+          >
             Colored borders
           </SelectItem>
-          <SelectItem className="" value="banana">
+          <SelectItem
+            onClick={() => updateTitle("Colored flashcards")}
+            className=""
+            value="flashcards"
+          >
             Colored flashcards
           </SelectItem>
-          <SelectItem value="blueberry">Not at all</SelectItem>
+          <SelectItem onClick={() => updateTitle("No color ")} value="No">
+            Not at all
+          </SelectItem>
         </SelectGroup>
       </SelectContent>
     </Select>
